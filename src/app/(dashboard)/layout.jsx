5aa45bd13b5c83e71346871da88faa48dd22185b
Megan Ring <meganbethring@gmail.com>
2023-11-16 22:46:32 -0800

    Add sidenav links

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,9 +1,23 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
+
 const DashboardLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute left-0 top-0 h-full w-64 border-r border-neutral-400">
-        Mood
+        <h2 className="text-2xl p-8">Mood</h2>
+        <ul className="gap-y-8 p-4">
+          {links.map(link => (
+            <li className="p-4" key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>)
+          )}
+        </ul>
       </aside>
       
       <div className="ml-64 h-full">
